88b5a1685a657a837794c77bb1fa7bd31d6172bf
Valeriy Litkovskyy <vlr.ltkvsk@protonmail.com>
2021-04-05 16:38:49 +0200

    better shell.nix

--- shell.nix
+++ shell.nix
@@ -1,7 +1,18 @@
-let nixpkgs = import <nixpkgs> {};
-in with nixpkgs; stdenv.mkDerivation {
+let
+  quickstart = builtins.fetchurl https://beta.quicklisp.org/quicklisp.lisp;
+  data-home = "\${XDG_DATA_HOME:-$HOME/.local/share}";
+in
+with import <nixpkgs> {};
+stdenv.mkDerivation {
   name = "common-lisp";
-  # clasp-common-lisp - could not build
-  # abcl does not work
   buildInputs = [ sbcl ecl ccl clisp ];
+  setupLispDev = writeShellScript "setupLispDev" ''
+    sbcl --non-interactive --load "${quickstart}" \
+         --eval "(quicklisp-quickstart:install :path #p\"${data-home}/quicklisp/\")"
+  '';
+  updateLispDev = writeShellScript "updateLispDev" ''
+    sbcl --non-interactive --eval "(ql:update-client)" --eval "(ql:update-dist \"quicklisp\")"
+  '';
+  shellHook = ''
+    '';
 }
