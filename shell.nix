cc854490db8900de75055c5f0f2fb03ef6850935
Valeriy Litkovskyy <vlr.ltkvsk@protonmail.com>
2021-11-18 18:51:50 +0100

    Improve shell.nix

--- shell.nix
+++ shell.nix
@@ -1,18 +1,49 @@
 let
-  quickstart = builtins.fetchurl https://beta.quicklisp.org/quicklisp.lisp;
-  data-home = "\${XDG_DATA_HOME:-$HOME/.local/share}";
-in
-with import <nixpkgs> {};
-stdenv.mkDerivation {
-  name = "common-lisp";
-  buildInputs = [ sbcl ecl ccl clisp ];
-  setupLispDev = writeShellScript "setupLispDev" ''
-    sbcl --non-interactive --load "${quickstart}" \
-         --eval "(quicklisp-quickstart:install :path #p\"${data-home}/quicklisp/\")"
+  pkgs = import <nixpkgs> {};
+
+  PROJECT_ROOT = builtins.toString ./.;
+  QUICKLISP_DIR = "${PROJECT_ROOT}/.quicklisp";
+
+  quicklisp-lisp = builtins.fetchurl https://beta.quicklisp.org/quicklisp.lisp;
+  quickstart = pkgs.writeShellScriptBin "quickstart" ''
+    ${pkgs.sbcl}/bin/sbcl \
+        --non-interactive \
+        --no-userinit \
+        --load "${quicklisp-lisp}" \
+        --eval "(quicklisp-quickstart:install :path #P\"${QUICKLISP_DIR}/\")"
   '';
-  updateLispDev = writeShellScript "updateLispDev" ''
-    sbcl --non-interactive --eval "(ql:update-client)" --eval "(ql:update-dist \"quicklisp\")"
+
+  init-lisp = pkgs.writeText "init.lisp" ''
+    #-quicklisp
+    (let ((quicklisp-init #P"${QUICKLISP_DIR}/setup.lisp"))
+      (when (probe-file quicklisp-init)
+        (load quicklisp-init)))
   '';
-  shellHook = ''
+
+  make-implementation = name: pkg: flags: pkgs.symlinkJoin {
+    name = name;
+    paths = [ pkg ];
+    nativeBuildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/${name} --add-flags '${flags}'
     '';
+  };
+
+  sbcl = make-implementation "sbcl" pkgs.sbcl "--userinit ${init-lisp}";
+  ecl = make-implementation "ecl" pkgs.ecl "--norc --load ${init-lisp}";
+  ccl = make-implementation "ccl" pkgs.ccl "--no-init --load ${init-lisp}";
+  clisp = make-implementation "clisp" pkgs.clisp "-norc -i ${init-lisp}";
+  abcl = make-implementation "abcl" pkgs.abcl "--noinit --load ${init-lisp}";
+
+in pkgs.mkShell {
+  CL_SOURCE_REGISTRY="${PROJECT_ROOT}:";
+  ASDF_OUTPUT_TRANSLATIONS = ''
+    (:output-translations
+     :ignore-inherited-configuration
+     (t ("${PROJECT_ROOT}" ".common-lisp" :implementation)))
+  '';
+  buildInputs =  [ quickstart sbcl ecl ccl clisp abcl ];
+  shellHook = ''
+    [ -d "${QUICKLISP_DIR}" ] || quickstart
+  '';
 }
